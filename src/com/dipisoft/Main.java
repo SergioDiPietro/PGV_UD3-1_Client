6af9aabdd9daf13b03c169659cd4eabf1dde56b3
Sergio Di Pietro <usr2264@salesianos-lacuesta.net>
2022-02-11 23:09:14 +0000

    Refactor & Feature: Codigo reestructurado. Añadida función de mensajes 'en vivo'.

--- src/com/dipisoft/Main.java
+++ src/com/dipisoft/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -17,18 +17,39 @@ public class Main {
 
         socket = new Socket("127.0.0.1", 8080);
 
-        oos = new ObjectOutputStream(socket.getOutputStream());
-        ois = new ObjectInputStream(socket.getInputStream());
-
         try {
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            ois = new ObjectInputStream(socket.getInputStream());
+
+            System.out.print("Username> ");
+            oos.writeObject(sc.nextLine());
+
+            System.out.println((String)ois.readObject());
+
+            String chatHistory = (String)ois.readObject();
+            if (chatHistory.isEmpty()) chatHistory = "<Chat vacío>";
+            System.out.println(chatHistory);
+
+            int messagesCount = chatHistory.length() - chatHistory.replace("[", "").length();
+
             while (true) {
-                String response = (String) ois.readObject();
-                System.out.println("<Server> " + response);
-                if (!response.equals("goodbye")) {
-                    System.out.print("> ");
-                    String text = sc.nextLine();
-                    oos.writeObject(text);
-                } else break;
+                System.out.print("> ");
+                String clientResponse = sc.nextLine();
+                oos.writeObject(clientResponse);
+                if (clientResponse.startsWith("message:")) messagesCount++;
+
+                String svResponse = (String) ois.readObject();
+                System.out.println("<Server> " + svResponse);
+                if (svResponse.equals("goodbye")) break;
+
+                oos.writeObject(messagesCount);
+
+                String newMessages = (String) ois.readObject();
+
+                if (!newMessages.isEmpty()) {
+                    System.out.println(newMessages);
+                    messagesCount += newMessages.length() - newMessages.replace("[", "").length();
+                }
             }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -36,9 +57,8 @@ public class Main {
             if (socket != null) socket.close();
             if (ois != null) ois.close();
             if (oos != null) oos.close();
-            System.out.println("\nConexión cerrada.");
+            System.out.println(">> Conexión finalizada.");
         }
-
         sc.close();
     }
 }
